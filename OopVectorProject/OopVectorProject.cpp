3a3e521a639cde9fff0450a095ec0a864b39a1c5
maxefimov <efimov.top.academy@gmail.com>
2023-10-08 12:31:25 +0300

    Добавьте файлы проекта.

---
+++ OopVectorProject/OopVectorProject.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class Vector
+{
+    int* array;
+    int size;
+public:
+    Vector() : array{ nullptr }, size{} {}
+    Vector(int size)
+        : array{ new int[size] }, size{ size } 
+    {
+        std::cout << this << " Vector construct\n";
+    }
+    ~Vector() 
+    { 
+        delete[] array; 
+        std::cout << this << " Vector destruct\n";
+    }
+};
+
+int main()
+{
+    {
+        Vector* v1 = new Vector(10);
+        Vector v2(10);
+        delete v1;
+    }
+    
+    //
+}
